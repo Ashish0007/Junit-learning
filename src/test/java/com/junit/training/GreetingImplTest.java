1669cb3af0211e54f2bea896111ceff94ce387b1
ashish.kumar <ashish.kumar@lab49.com>
2017-02-06 18:14:17 +0530

    refine test class

--- src/test/java/com/junit/training/GreetingImplTest.java
+++ src/test/java/com/junit/training/GreetingImplTest.java
@@ -1,14 +1,20 @@
 package com.junit.training;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GreetingImplTest {
 
+    GreetingImpl greeting;
+    @Before
+    public void setUp(){
+         greeting = new GreetingImpl();
+    }
     @Test
     public void testGreet() {
 
-        GreetingImpl greeting = new GreetingImpl();
+
         String result = greeting.greet("unit-test");
         Assert.assertNotNull(result);
         Assert.assertTrue("Hello unit-test".equals(result));
@@ -16,7 +22,6 @@ public class GreetingImplTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void greet_should_throw_illegalArguementException(){
-        Greeting greeting = new GreetingImpl();
         greeting.greet(null);
     }
 }
